bb9fd5fe4ee2c98a627069ec762bb3b7650a4cda
Dennis Mattews <alttert@mail.ru>
2024-03-14 01:50:52 +0300

    extends todo list ver. 0.1

--- django_serwal/urls.py
+++ django_serwal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html")),
+    path("", TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
 ]
