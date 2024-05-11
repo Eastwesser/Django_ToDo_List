67b6716ffb6d03737666d8895a5968be07cd2595
Dennis Mattews <alttert@mail.ru>
2024-05-11 21:46:55 +0300

    Django CBV 2

--- django_serwal/urls.py
+++ django_serwal/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("todos/", include("todo_list.urls")),
     path("admin/", admin.site.urls),
 ]
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+app_name = "todo_list"
+
 urlpatterns = [
     path(
         "",
