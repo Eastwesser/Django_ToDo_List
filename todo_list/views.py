0ddcaafa63cb3025a48cdf299a5e967a8074ca2b
Dennis Mattews <alttert@mail.ru>
2024-05-12 03:05:51 +0300

    Django CBV 4

--- todo_list/views.py
+++ todo_list/views.py
@@ -6,4 +6,14 @@ from django.shortcuts import render
 
 
 def index_view(request: HttpRequest) -> HttpResponse:
-    return render(request, template_name="todo_list/index.html")
+    todo_items = [
+        "Item 1",
+        "item 2",
+        "Item 3",
+        "Item 4",
+    ]
+    return render(
+        request,
+        template_name="todo_list/index.html",
+        context={"todo_items": todo_items},
+    )
