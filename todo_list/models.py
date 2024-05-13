2141389e5bf4aa2c869b400f15e6fa44caf2ccd2
Dennis Mattews <alttert@mail.ru>
2024-05-13 03:34:46 +0300

    Django CBV 5

--- todo_list/models.py
+++ todo_list/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ToDoItem(models.Model):
     class Meta:
+        ordering = ("id", )
         verbose_name = "ToDo Item for 'SeRWaL' project"
 
     title = models.CharField(max_length=250)
--- todo_list/views.py
+++ todo_list/views.py
@@ -3,17 +3,31 @@ from django.http import (
     HttpRequest,
 )
 from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    ListView,
+)
+
+from .models import ToDoItem
 
 
 def index_view(request: HttpRequest) -> HttpResponse:
-    todo_items = [
-        "Item 1",
-        "item 2",
-        "Item 3",
-        "Item 4",
-    ]
+    todo_items = ToDoItem.objects.all()
     return render(
         request,
         template_name="todo_list/index.html",
         context={"todo_items": todo_items},
     )
+
+
+class ToDoListIndexView(TemplateView):
+    template_name = "todo_list/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_items"] = ToDoItem.objects.all()
+        return context
+
+
+class ToDoListView(ListView):
+    template_name = "todo_list/index.html"
